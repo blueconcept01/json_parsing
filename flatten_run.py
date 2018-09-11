fae55f3edc30fd89be0a0bfae455b6b45ef9f52b
blueconcept01 <31360801+blueconcept01@users.noreply.github.com>
2018-09-11 04:06:32 -0700

    added parsed files

--- flatten_run.py
+++ flatten_run.py
@@ -2,4 +2,7 @@ import sys
 import object_flattening.flatten_file as flatten_file
 
 if __name__ == '__main__':
-    flatten_file.flatten_json_file(sys.argv[1])
+    if len(sys.argv) == 2:
+        flatten_file.flatten_json_file(sys.argv[1])
+    else:
+        print("Pass in a file name argument")
